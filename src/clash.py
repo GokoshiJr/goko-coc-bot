360dd89bf2f958bced53e9bc399d3cf6769c2f88
GokoshiJr <julioagonzalez18@gmail.com>
2021-02-15 16:34:29 -0400

    feat: Add war_time logic

--- src/clash.py
+++ src/clash.py
@@ -16,7 +16,11 @@ async def donaciones(client, clan_tag, negativas):
 async def war(client, clan_tag) -> str:
   try:
     war = await client.get_clan_war(clan_tag)    
-    result = f"{war.clan.name} vs {war.opponent.name} ({war.type}) \nTime: \nTamaño: {war.team_size} \nEstatus: {war.state} {war.status} \nMiembros en guerra: \n{miembros(war.members)}"
+    result = \
+    f"{war.clan.name} vs {war.opponent.name} ({war.type})" \
+    f"\nTamaño: {war.team_size} \nEstatus: {war.state} {war.status} " \
+    f"\nMiembros en guerra: \n{miembros(war.members)} \n" \
+    f"{war_time(war)}"    
   except coc.PrivateWarLog:
     result = "El clan posee el registro de guerras privado"
   return result
@@ -26,4 +30,15 @@ def miembros(members) -> str:
   for member in members:
     if not(member.is_opponent):
       result += f"{member.map_position}. {member.name} TH {member.town_hall} \n"
+  return result
+
+def war_time(war) -> str:
+  time_to_start = str(war.start_time.time - war.end_time.now)
+  time_to_end = war.end_time.time - war.end_time.now
+  result = ""  
+  if (time_to_end.days == 1):
+    result = f"Tiempo para que inicie: {time_to_start[0]} horas {time_to_start[2:4]} minutos."
+  else:    
+    time_to_end = str(time_to_end)
+    result = f"Tiempo para que finalice: {time_to_end[0]} horas {time_to_end[2:4]} minutos."
   return result
